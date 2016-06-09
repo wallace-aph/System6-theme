af2135017b4ef85b42c56713d44b113661e2aba0
Wallace <ftubuntu@gmail.com>
2016-06-09 00:15:02 -0400

    lolforgotquotes

--- resources/lemonbar/runme.sh
+++ resources/lemonbar/runme.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "make sure you use a xft supporing lemonbar and put Chicago_15.ttf in your ~/.fonts folder! also, patch lemonbar with the borders patch for best results.
+echo "make sure you use a xft supporing lemonbar and put Chicago_15.ttf in your ~/.fonts folder! also, patch lemonbar with the borders patch for best results."
 bash ./bar | lemonbar -f "Chicago_15" -g 1366x20 -d -B "#ffffff" -r 1
